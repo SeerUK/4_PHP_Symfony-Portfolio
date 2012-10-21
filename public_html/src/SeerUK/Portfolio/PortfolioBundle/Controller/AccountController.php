211e8141e8191387d5a9232130c2efd46cc7e05a
Elliot Wright <wright.elliot@gmail.com>
2012-10-21 17:51:42 +0100

    Login system working (in memory users currently).

--- public_html/src/SeerUK/Portfolio/PortfolioBundle/Controller/AccountController.php
+++ public_html/src/SeerUK/Portfolio/PortfolioBundle/Controller/AccountController.php
@@ -3,6 +3,7 @@
 namespace SeerUK\Portfolio\PortfolioBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
 
 use SeerUK\Portfolio\PortfolioBundle\DependencyInjection\ExtendedController;
 
@@ -11,8 +12,22 @@ class AccountController extends ExtendedController
 
 	public function loginAction()
 	{
+		$request = $this->getRequest();
+		$session = $request->getSession();
+
+		if($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR))
+		{
+			$error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+		}
+		else
+		{
+			$error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+			$session->remove(SecurityContext::AUTHENTICATION_ERROR);
+		}
+
     	$responseVariables = [
     		'pageTitle' => 'Login',
+    		'error'     => $error,
     	];
         return $this->render('SeerUKPortfolioBundle:Account:Login.html.twig', $responseVariables);
 	}
