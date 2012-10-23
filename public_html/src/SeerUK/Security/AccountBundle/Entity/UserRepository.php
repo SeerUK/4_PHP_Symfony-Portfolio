4cbfc58173692cc64675960c2f8228c11fa49bd6
Elliot Wright <wright.elliot@gmail.com>
2012-10-23 22:31:35 +0100

    Using Doctrine's memcache driver now instead. Silly me...

--- public_html/src/SeerUK/Security/AccountBundle/Entity/UserRepository.php
+++ public_html/src/SeerUK/Security/AccountBundle/Entity/UserRepository.php
@@ -9,37 +9,26 @@ use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\NoResultException;
 
-use SeerUK\Portfolio\PortfolioBundle\DependencyInjection\Cache\MemcacheClient;
-
 class UserRepository extends EntityRepository implements UserProviderInterface
 {
 	public function loadUserByUsername($username)
     {
-        $memcacheClient = new MemcacheClient;
-
         $ck = get_class($this) . '::' . $username;
-        $cq = $memcacheClient->get($ck);
 
-        if(!empty($cq))
-        {
-            $user = $cq;
-        }
-        else
-        {
-            $q = $this
-                ->createQueryBuilder('u')
-                ->where('u.account_name = :username OR u.account_email = :email')
-                ->setParameter('username', $username)
-                ->setParameter('email', $username)
-                ->getQuery()
-            ;
+        $q = $this
+            ->createQueryBuilder('u')
+            ->where('u.account_name = :username OR u.account_email = :email')
+            ->setParameter('username', $username)
+            ->setParameter('email', $username)
+            ->getQuery()
+        ;
 
-            try {
-                $user = $q->getSingleResult();
-                $memcacheClient->set($ck, $user, 60);
-            } catch (NoResultException $e) {
-                throw new UsernameNotFoundException(sprintf('Unable to find an active user AcmeUserBundle:User object identified by "%s".', $username), null, 0, $e);
-            }
+        $q->useResultCache(true, 120, $ck);
+
+        try {
+            $user = $q->getSingleResult();
+        } catch (NoResultException $e) {
+            throw new UsernameNotFoundException(sprintf('Unable to find an active user AcmeUserBundle:User object identified by "%s".', $username), null, 0, $e);
         }
 
         return $user;
